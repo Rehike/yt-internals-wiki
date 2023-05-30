bb7bcaae7524118db2c74c538e746445ebe02d0d
Daylin Cooper <dcoop2004@gmail.com>
2023-05-29 17:10:08 -0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -26,24 +26,17 @@ function renderPageMarkdown(mdDoc)
     return markdownit.render(mdDoc);
 }
 
-(async () => {
-    let sidebar = await fetch("sidebar.json");
-    let response = await sidebar.json();
-
-    if (response)
-    {
-        document.querySelector("#guide").innerHTML =
-            nunjucks.render("sidebar.html", response);
-    }
-})();
-
-window.addEventListener("hashchange", async (e) => {
+async function downloadAndRefreshPage(showProgressBar = false) {
     let page = (PAGENAME_REGEX.exec(e.newURL))[1];
-    let progress = document.querySelector("#progress");
+    
+    if (showProgressBar)
+    {
+        let progress = document.querySelector("#progress");
 
-    progress.style.transitionDuration = ".9s";
-    progress.style.width = "75%";
-    progress.style.opacity = "1";
+        progress.style.transitionDuration = ".9s";
+        progress.style.width = "75%";
+        progress.style.opacity = "1";
+    }
 
     let response = await fetch("pages/" + page + ".md");
     let responseText = await response.text();
@@ -56,14 +49,17 @@ window.addEventListener("hashchange", async (e) => {
     {
         document.querySelector("#content").innerHTML = renderPageMarkdown(responseText);
     }
-
-    progress.style.transitionDuration = ".5s";
-    progress.style.width = "100%";
-    progress.addEventListener("transitionend", function temp() {
-        progress.removeEventListener("transitionend", temp);
-        progress.style.opacity = "0";
-        progress.style.width = "0%";
-    });
+    
+    if (showProgressBar)
+    {
+        progress.style.transitionDuration = ".5s";
+        progress.style.width = "100%";
+        progress.addEventListener("transitionend", function temp() {
+            progress.removeEventListener("transitionend", temp);
+            progress.style.opacity = "0";
+            progress.style.width = "0%";
+        });
+    }
 
     if (document.querySelector(".guide-item.selected") != null)
     {
@@ -71,15 +67,22 @@ window.addEventListener("hashchange", async (e) => {
     }
     
     document.querySelector(`.guide-item-link[href$="${location.hash}"]`).parentElement.classList.add("selected");
-});
+}
+
+(async () => {
+    let sidebar = await fetch("sidebar.json");
+    let response = await sidebar.json();
+
+    if (response)
+    {
+        document.querySelector("#guide").innerHTML =
+            nunjucks.render("sidebar.html", response);
+    }
+})();
+
+window.addEventListener("hashchange", async e => await downloadAndRefreshPage(true));
 
 if (location.hash.length > 2)
 (async () => {
-    let page = (PAGENAME_REGEX.exec(location.hash))[1];
-    let html = await fetch("pages/" + page + ".md");
-    html = await html.text();
-    document.querySelector("#content").innerHTML = html;
-
-    let item = await waitForElm(`.guide-item-link[href$="${location.hash}"]`);
-    item.parentElement.classList.add("selected");
+    downloadAndRefreshPage();
 })();
